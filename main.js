7510ab2f1ab07298a480ec64abd98d9ee238d7a2
MATX-sys2244 <146489499+MATX-sys2244@users.noreply.github.com>
2024-01-17 08:36:01 -0300

    Add files via upload
    code for my project IMC. 
    I used 3 language.
     HTML, JavaScript and CSS
    Is not finished, but is necessary updates on the code

---
+++ main.js
@@ -0,0 +1,7 @@
+    const weight = document.getElementById('weight').value
+    const height = document.getElementById('height').value
+    const IMC = weight/ (height*height)
+
+    function click{
+        
+    }
